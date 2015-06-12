8496b38937f604c0ac1f2e7201e706d4e0e0ac6c
David Lehnen <dl@interactive-pioneers.de>
2015-06-12 15:02:20 +0200

    Add tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -14,7 +14,7 @@
     describe('init', function() {
 
       beforeEach(function() {
-        object = $('input').iptValidator(config);
+        object = $('form').iptValidator(config);
       });
 
       it('expected to construct object', function() {
@@ -27,10 +27,90 @@
 
     });
 
+    describe('_isEmpty', function() {
+
+      beforeEach(function() {
+        object = $('form').iptValidator();
+      });
+
+      it('expected to return false', function() {
+        return expect(object.data(pluginName)._isEmpty('test')).to.not.be.ok;
+      });
+
+      it('expected to return true', function() {
+        return expect(object.data(pluginName)._isEmpty(' ')).to.be.ok;
+      });
+
+    });
+
+    describe('_isValidEmail', function() {
+
+      beforeEach(function() {
+        object = $('form').iptValidator();
+      });
+
+      it('expected to return false', function() {
+        return expect(object.data(pluginName)._isValidEmail('test')).to.not.be.ok;
+      });
+
+      it('expected to return true', function() {
+        return expect(object.data(pluginName)._isValidEmail('test@test.de')).to.be.ok;
+      });
+
+    });
+
+    describe('_isValidPhone', function() {
+
+      beforeEach(function() {
+        object = $('form').iptValidator();
+      });
+
+      it('expected to return false', function() {
+        return expect(object.data(pluginName)._isValidPhone('Test')).to.not.be.ok;
+      });
+
+      it('expected to return true', function() {
+        return expect(object.data(pluginName)._isValidPhone('+49 211 / 123123 - 0')).to.be.ok;
+      });
+
+    });
+
+    describe('_isValidPostcode', function() {
+
+      beforeEach(function() {
+        object = $('form').iptValidator();
+      });
+
+      it('expected to return false', function() {
+        return expect(object.data(pluginName)._isValidPostcode('-')).to.not.be.ok;
+      });
+
+      it('expected to return true', function() {
+        return expect(object.data(pluginName)._isValidPostcode('DE123 - 56')).to.be.ok;
+      });
+
+    });
+
+    describe('_isValidNumeric', function() {
+
+      beforeEach(function() {
+        object = $('form').iptValidator();
+      });
+
+      it('expected to return false', function() {
+        return expect(object.data(pluginName)._isValidNumeric('Test')).to.not.be.ok;
+      });
+
+      it('expected to return true', function() {
+        return expect(object.data(pluginName)._isValidNumeric('10.00')).to.be.ok;
+      });
+
+    });
+
     describe('destroy', function() {
 
       beforeEach(function() {
-        object = $('input').iptValidator();
+        object = $('form').iptValidator();
       });
 
       it('expected to remove data', function() {
