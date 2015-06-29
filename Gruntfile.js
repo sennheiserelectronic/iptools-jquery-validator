684855e6c981d47d8d01e53a69aa9d62a34ce1ba
Ain Tohvri <at@interactive-pioneers.de>
2015-06-29 10:59:47 +0200

    Add JSCS to QA stack

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function(grunt) {
     yeoman: {
       src: 'src',
       dist: 'dist',
+      test: 'test',
       pkg: grunt.file.readJSON('package.json'),
       meta: {
         banner: '/*! <%= yeoman.pkg.name %> - v<%= yeoman.pkg.version %> - ' +
@@ -59,6 +60,7 @@ module.exports = function(grunt) {
       qa: {
         tasks: [
           'jshint',
+          'jscs',
           'mocha'
         ]
       },
@@ -94,6 +96,20 @@ module.exports = function(grunt) {
         files: {
           'dist/iptools-jquery-validator.css' : 'src/iptools-jquery-validator.scss'
         }
+      }
+    },
+    jscs: {
+      options: {
+        config: '.jscsrc',
+        esnext: false,
+        verbose: true
+      },
+      files: {
+        src: [
+          'Gruntfile.js',
+          '<%= yeoman.test %>/spec/*.js',
+          '<%= yeoman.src %>/*.js'
+        ]
       }
     }
   });
